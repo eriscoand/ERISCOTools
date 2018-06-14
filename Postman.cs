36828c6534ef9a2e512697d2ee6de099b04824c9
Eric Risco de la Torre <erisco@icloud.com>
2018-06-15 01:08:43 +0200

    Postman

---
+++ Postman.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERISCOTools
+{
+    public class Postman
+    {
+        private string URL;
+        public Postman(string URL)
+        {
+            this.URL = URL;
+        }
+
+        public void POST(string json)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
